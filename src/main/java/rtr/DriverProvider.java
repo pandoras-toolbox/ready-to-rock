9c4f994f4416e4107f3dbc94dbc8e2695fd773fa
Zeljko Markovic <zeljko.markovic@hotmail.ch>
2020-08-19 11:05:57 +0200

    Fixed setting the binary to ChromeOptions

--- src/main/java/rtr/DriverProvider.java
+++ src/main/java/rtr/DriverProvider.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.io.File;
+
 // Needs to be thread-safe because of parallel test execution.
 public enum DriverProvider {
 
@@ -21,14 +23,13 @@ public enum DriverProvider {
             switch (browser) {
                 case CHROME -> {
                     WebDriverManager.chromedriver().setup();
-                    ChromeOptions options = new ChromeOptions();
-                    options.setBinary("");
-                    webDriver = new ChromeDriver();
+                    ChromeOptions options = createChromeOptions(config.getChromeBinary().orElse(null));
+                    webDriver = new ChromeDriver(options);
                 }
                 case CHROME_HEADLESS -> {
                     WebDriverManager.chromedriver().setup();
                     // See: https://peter.sh/experiments/chromium-command-line-switches/
-                    ChromeOptions options = new ChromeOptions();
+                    ChromeOptions options = createChromeOptions(config.getChromeBinary().orElse(null));
                     options.addArguments("--headless");
                     webDriver = new ChromeDriver(options);
                 }
@@ -43,6 +44,14 @@ public enum DriverProvider {
             }
         }
         return webDriver;
+    }
+
+    private ChromeOptions createChromeOptions(File chromeBinary) {
+        ChromeOptions options = new ChromeOptions();
+        if (chromeBinary != null) {
+            options.setBinary(chromeBinary);
+        }
+        return options;
     }
 
 }
